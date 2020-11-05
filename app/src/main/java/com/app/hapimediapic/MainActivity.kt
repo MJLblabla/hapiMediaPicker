6c1d262f8f3c30d731c026a6e505204b46b65e9f
mjl <1013666753@qq.com>
2020-11-05 16:55:23 +0800

    temo

--- app/src/main/java/com/app/hapimediapic/MainActivity.kt
+++ app/src/main/java/com/app/hapimediapic/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.app.hapimediapic
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -21,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         // 选择图片　默认选择弹窗
         button.setOnClickListener {
             mPicPickHelper.show(Size(1,1), object : ImagePickCallback {
-                override fun onSuccess(result: String?) {
+                override fun onSuccess(result: String?,uri: Uri?) {
                     Toast.makeText(this@MainActivity, "文件路径${result}", Toast.LENGTH_SHORT).show()
                 }
 
@@ -31,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         // 选择图片从　相机
         button3.setOnClickListener {
             mPicPickHelper.fromCamera(null, object : ImagePickCallback {
-                override fun onSuccess(result: String?) {
+                override fun onSuccess(result: String?,uri: Uri?) {
                     Toast.makeText(this@MainActivity, "文件路径${result}", Toast.LENGTH_SHORT).show()
                 }
 
@@ -41,7 +42,7 @@ class MainActivity : AppCompatActivity() {
         // 选择图片从　图库
         button2.setOnClickListener {
             mPicPickHelper.fromLocal(Size(1,1), object : ImagePickCallback {
-                override fun onSuccess(result: String?) {
+                override fun onSuccess(result: String?,uri: Uri?) {
                     Toast.makeText(this@MainActivity, "文件路径${result}", Toast.LENGTH_SHORT).show()
                 }
             })
